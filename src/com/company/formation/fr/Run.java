0aa7b66f0c0e9b26a17217b4f1f8594a99889277
dl3cO1n <david.lecoin@yahoo.com>
2021-10-04 15:26:12 +0200

    point3D

--- src/com/company/formation/fr/Run.java
+++ src/com/company/formation/fr/Run.java
@@ -1,16 +1,9 @@
 package com.company.formation.fr;
-
-import com.company.formation.fr.dessin.Point2D;
+import com.company.formation.fr.dessin.Point3D;
 
 public class Run {
     public static void main(String[] args){
-        Point2D point1 = new Point2D(4, 4);
-        Point2D point2 = new Point2D(5, 5);
-
-        point1.displayPoint();
-        point1.translate(5, 5);
-        point2.displayPoint();
-        point2.translate(6, 6);
-        System.out.println(Point2D.cpt);
+        Point3D point = new Point3D(8, 4, 4);
+        point.displayPoint();
     }
 }
